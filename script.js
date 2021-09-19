cf02d0d39cedacdbd4df59b4d813a4f9d34d6196
Ahos333 <anghos080@gmail.com>
2021-09-19 16:08:30 -0500

    updated project; fixed bugs; the names, ids, and links to my Github repositories are displayed;

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 //repo link: https://api.github.com/users/ahos333/repos
-$(document).ready(function() {
-    $(".icon").hover(function(){
-        $(this).css("fontSize", "2.5rem");
-        }, function(){
-        $(this).css("fontSize", "2rem");
-      });
-});
 
 let newRequest = new XMLHttpRequest();
-newRequest.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) {
-        let repoObj = JSON.parse(this.responseText);
-        let repoContainer = document.getElementById("repo-item");
-        for (i = 0; i < repoContainer.length; i++) {
-            document.getElementById('repo-name').innerHTML = "Project Name: " + repoObj[0].name;
-            document.getElementById('repo-url').innerHTML = "Link: " +  repoObj[1].html_url;
-        }
-        console.log(repoObj);
-        console.log(repoObj[1].html_url);
-        
-        
-    } 
-}
+      newRequest.onreadystatechange = function(){
+          if (this.readyState == 4 && this.status == 200) {
+              let repoObj = JSON.parse(this.response);
+              for (i = 0; i < repoObj.length; i++) {
+                let repoSection = `
+                                    <h3 class="repo-name">Project Name: ${repoObj[i].name}</h3>
+                                    <h3 class="repo-id">Project Id: ${repoObj[i].id}</h3>
+                                    <a class="btn repo-btn" href="${repoObj[i].html_url}">Go to Project</a>
+                                    `
+                let repoNode = document.createElement("li");
+                repoNode.classList.add("repo-item");
+                document.querySelector(".repo-section").appendChild(repoNode);
+                repoNode.innerHTML = repoSection;
+              }
+              
+          }
+              
+      } 
+    newRequest.open('GET', 'https://api.github.com/users/ahos333/repos', true);
+    newRequest.send();
 
-newRequest.open("GET", "https://api.github.com/users/ahos333/repos", true);
-newRequest.send();
+
+/*
+
+let newRequest = new XMLHttpRequest();
+      newRequest.onreadystatechange = function(){
+          if (this.readyState == 4 && this.status == 200) {
+              let repoObj = JSON.parse(this.response);
+              for (i = 0; i < repoObj.length; i++) {
+                let repoSection = `
+                                    <h3 class="repo-name">Project Name: ${repoObj[i].name}</h3>
+                                    <h3 class="repo-id">Project Id: ${repoObj[i].id}</h3>
+                                    `
+                let repoNode = document.createElement("li");
+                repoNode.classList.add("repo-item");
+                document.querySelector(".repo-section").appendChild(repoNode);
+                repoNode.innerHTML = repoSection;
+              }
+              
+          }
+              
+      } 
+    newRequest.open('GET', 'https://api.github.com/users/ahos333/repos', true);
+    newRequest.send();
+
+*/
